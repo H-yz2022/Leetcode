a978e6abe545c07cc46f66a63657df6c8039835c
Yunzhu Huang <110589796+H-yz2022@users.noreply.github.com>
2026-06-02 00:13:07 +0800

    Update SQL50.sql

--- src/SQL50.sql
+++ src/SQL50.sql
@@ -1,1 +1,27 @@
+# Select 1
+SELECT
+    product_id
+FROM
+    Products
+WHERE
+    low_fats = 'Y' AND recyclable = 'Y'
+
+# Select 2
+SELECT
+    name
+FROM
+    Customer
+WHERE
+    referee_id != 2 OR referee_id IS NULL
+
+# Select 3
+SELECT 
+    name, population, area
+FROM 
+    World
+WHERE
+    area >= 3000000 OR population >= 25000000
+
+
+
 
