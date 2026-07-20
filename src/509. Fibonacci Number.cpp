a3755ad6d93db6e320f031d9e6e56ca2be8ade6b
Yunzhu Huang <110589796+H-yz2022@users.noreply.github.com>
2026-07-21 00:17:24 +0800

    Update 509. Fibonacci Number.cpp

--- src/509. Fibonacci Number.cpp
+++ src/509. Fibonacci Number.cpp
@@ -1,3 +1,14 @@
+// Fibonacci numbers: given n, compute
+// F_n = F_n-1 + F_n-2 ...
+// F_1 = F_2 = 1
+// Subprobs: F(i) = F_i 1 <= i <= n
+// Relate: F(i) = F(i-1) + F(i-2)
+// Topological order: increasing i, for i = 1, i = 2
+// Base: F(1) = F(2) = 1
+// Original: F(n)
+// Time: T(n) = T(n-1) + T(n-2) + 1
+
+
 class Solution {
 public:
     int fib(int n) {
