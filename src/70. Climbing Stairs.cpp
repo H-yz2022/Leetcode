e057e42b995178ae03db92b7aced4cfb9cb1b009
Yunzhu Huang <110589796+H-yz2022@users.noreply.github.com>
2026-09-09 16:57:45 -0400

    Update 70. Climbing Stairs.cpp

--- src/70. Climbing Stairs.cpp
+++ src/70. Climbing Stairs.cpp
@@ -3,8 +3,7 @@ public:
     int climbStairs(int n) {
         if (n == 0 || n == 1) {
             return 1;
-        }
-        return climbStairs(n-1) + climbStairs(n-2);
-        
+        }//base cases
+        return climbStairs(n-1) + climbStairs(n-2);//relation + original
     }
 };
