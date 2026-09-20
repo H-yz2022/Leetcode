b11b19a7a894c4185ceab1c8b4fb3ffb26fac869
Yunzhu Huang <110589796+H-yz2022@users.noreply.github.com>
2026-09-19 23:55:21 -0400

    Implement brute force and hash table solutions for Two Sum

--- src/1. Two Sum.cpp
+++ src/1. Two Sum.cpp
@@ -1,3 +1,21 @@
+//straight=forward
+// one pointer -->return the index
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // No solution found
+    }
+};
+
+//hash table
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
