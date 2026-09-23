8a68849b7f27efe68394956fbc4ccf84f282f162
Yunzhu Huang <110589796+H-yz2022@users.noreply.github.com>
2026-09-22 22:49:33 -0400

    Implement isValid method for parentheses validation

---
+++ src/20.Validparenthese.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+class Solution {
+  public boolean isValid(String s) {
+    if (s.length() == 0){
+      return true;
+    }
+    Stack<Character> stack = new Stack<>();
+    for (char ch: s.toCharArray()) {
+      if (ch== '(' || ch =='{' || ch == '['){
+        stack.push(ch);
+      } else {
+        if (stack.isEmpty()) {
+          return false;
+        } else {
+          char temp =stack.pop();
+          if (ch==')'){
+            if (temp!='('){
+              return false;
+            }
+          } else if (ch=='}'){
+            if (temp!='{'){
+              return false;
+            }
+          } else if (ch==']'){
+            if (temp!='['){
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return stack.isEmpty()? true : false;
+  }  
+}
