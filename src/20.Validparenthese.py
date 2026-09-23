4002529789e80bac7fa2b143caff426296f882f1
Yunzhu Huang <110589796+H-yz2022@users.noreply.github.com>
2026-09-22 22:40:12 -0400

    Fix indentation and formatting in isValid function

--- src/20.Validparenthese.py
+++ src/20.Validparenthese.py
@@ -6,8 +6,8 @@ class Solution:
   def isValid(self, s: str) -> bool:
     if len(s) == 0:
       return True
-      stack = []
-      for c in s:
+    stack = []
+    for c in s:
         if c == '(' or c== '[' or c == '{':
           stack.append(c)
         else:
@@ -24,4 +24,4 @@ class Solution:
               elif c == '}':
                 if temp!='{':
                   return False
-       return True if len(stack) == 0 else False
+    return True if len(stack) == 0 else False
